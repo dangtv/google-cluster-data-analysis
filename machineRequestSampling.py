c1c781cf810da97c051c8feb481b8ed5c7e747ce
Dang Tran <dangtv18@gmail.com>
2016-03-29 16:33:31 +0700

    optimize code for machine request sampling

--- machineRequestSampling.py
+++ machineRequestSampling.py
@@ -34,6 +34,11 @@ task_events_csv_colnames = ['time', 'missing', 'job_id', 'task_idx', 'machine_id
                             'priority', 'cpu_requested', 'mem_requested', 'disk', 'restriction']
 
 totalreadfile =0
+
+cpu_request = 0.0
+mem_request = 0.0
+disk_request = 0.0
+number_of_task_requested = 0
 for fn in sorted(listdir(data_directory)):
 
     fp = path.join(data_directory, fn)
@@ -46,14 +51,15 @@ for fn in sorted(listdir(data_directory)):
     for index, event in task_events_df.iterrows():
 
         while current_sample_moment is not None and event['time'] > current_sample_moment:
-            tmp_tasks_df = DataFrame(tasks_dict.values())
-            tmp_tasks_df = tmp_tasks_df[tmp_tasks_df['machine_id']==machine_id]
+            # tmp_tasks_df = DataFrame(tasks_dict.values())
+            # tmp_tasks_df = tmp_tasks_df[tmp_tasks_df['machine_id']==machine_id]
             # dong duoi nay co the toi uu hon
             samples_dicts[current_sample_moment] = ({'time' : current_sample_moment,
-                                                     'cpu_requested' : (tmp_tasks_df['cpu_requested']).sum(),
-                                                     'mem_requested' : (tmp_tasks_df['mem_requested']).sum(),
-                                                     'disk_space_requested': (tmp_tasks_df['disk']).sum(),
-                                                     'number_of_task_requested': len(tmp_tasks_df['machine_id'])})
+                                                     'cpu_requested' : cpu_request,
+                                                     'mem_requested' : mem_request,
+                                                     'disk_space_requested': disk_request,
+                                                     'number_of_task_requested': number_of_task_requested})
+
             try:
                 current_sample_moment = next(sample_moments_iterator)
             except StopIteration:
@@ -62,13 +68,48 @@ for fn in sorted(listdir(data_directory)):
 
         # if event['machine_id']== machine_id:
         if event['event_type'] in [0, 7, 8]:
+            if tasks_dict.has_key((event['job_id'], event['task_idx'])):
+                if tasks_dict[(event['job_id'], event['task_idx'])]['machine_id'] == machine_id:
+                    if tasks_dict[(event['job_id'], event['task_idx'])]['cpu_requested']>0:
+                        cpu_request = cpu_request - tasks_dict[(event['job_id'], event['task_idx'])]['cpu_requested']
+
+                    if tasks_dict[(event['job_id'], event['task_idx'])]['mem_requested']>0:
+                        mem_request = mem_request - tasks_dict[(event['job_id'], event['task_idx'])]['mem_requested']
+
+                    if tasks_dict[(event['job_id'], event['task_idx'])]['disk']>0:
+                        disk_request = disk_request - tasks_dict[(event['job_id'], event['task_idx'])]['disk']
+                    number_of_task_requested = number_of_task_requested -1
+
+            if event['machine_id'] == machine_id:
+                if event['cpu_requested']>0:
+                    cpu_request = cpu_request + event['cpu_requested']
+
+                if event['mem_requested']>0:
+                    mem_request = mem_request + event['mem_requested']
+
+                if event['disk']>0:
+                    disk_request = disk_request + event['disk']
+                number_of_task_requested = number_of_task_requested+1
+
             tasks_dict[(event['job_id'], event['task_idx'])] = {'task_id' : (event['job_id'], event['task_idx']),
                                                                     'machine_id' : event['machine_id'],
                                                                     'cpu_requested' : event['cpu_requested'],
                                                                     'mem_requested' : event['mem_requested'],
                                                                     'disk': event['disk']}
+
         elif event['event_type'] in [2, 3, 4, 5, 6]:
             try:
+                if tasks_dict.has_key((event['job_id'], event['task_idx'])):
+                    if tasks_dict[(event['job_id'], event['task_idx'])]['machine_id'] == machine_id:
+                        if tasks_dict[(event['job_id'], event['task_idx'])]['cpu_requested']>0:
+                            cpu_request = cpu_request - tasks_dict[(event['job_id'], event['task_idx'])]['cpu_requested']
+
+                        if tasks_dict[(event['job_id'], event['task_idx'])]['mem_requested']>0:
+                            mem_request = mem_request - tasks_dict[(event['job_id'], event['task_idx'])]['mem_requested']
+
+                        if tasks_dict[(event['job_id'], event['task_idx'])]['disk']>0:
+                            disk_request = disk_request - tasks_dict[(event['job_id'], event['task_idx'])]['disk']
+                        number_of_task_requested = number_of_task_requested -1
                 del tasks_dict[(event['job_id'], event['task_idx'])]
             except:
                 print "loi task ("+ str(event['job_id'])+", "+\
